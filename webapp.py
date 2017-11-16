e0846bd70288feaf3a9cf2fcee5926e702cc2e83
f.c.lopez <casmali.f.lopez@gmail.com>
2017-11-16 10:12:46 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,20 +5,29 @@ import json
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 with open("state_fragility.json") as fragility_data:
-        counties = json.load(fragility_data)
+        states = json.load(fragility_data)
 
 @app.route("/")
 def render_main():
-    return render_template('index.html')
+    return render_template('index.html',part = getStateOptions(states))
 @app.route("/fir")
 def render_page1():
-    return render_template('first.html')
+    return render_template('first.html',)
 @app.route("/sec")
 def render_page2():
     return render_template('second.html')
 @app.route("/thi")
 def render_page3():
     return render_template('third.html')
+
+def getStateOptions(states):
+    ret = ""
+    lis = {"doestmantter":True}
+    for i in counties:
+        if not(i["State"] in lis):
+            ret += Markup("<option value=\"" + i['State'] + "\">" + i['State'] + "</option>")
+            lis[i['State']] = True
+    return ret
 
 @app.route("/response")
 def render_response():
