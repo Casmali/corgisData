3e9a4aa06d0731c8adcfb552ec450cfbc8624744
f.c.lopez <casmali.f.lopez@gmail.com>
2017-11-30 09:25:07 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -39,19 +39,29 @@ def getYearOptions(states):
 
 @app.route("/response")
 def render_response():
-        ins = float(request.args['inches'])
-        #The request object stores information that was sent by the client to the server.
-        #the args is a multidict
-        #the way we get info from args is that it is visible in a url. - the information in args is visible in the url for hte page being requested(ex. .../response?color=blue)
-        res = str(ins*2.54)
-        return render_template('response.html', their = request.args['inches'], response = res)
+        na = request.args['nameof']
+        ye = str(request.args['year'])
+        count = states[0]
+        for i in states:
+                if i['Country'] == na and i['Year'] = ye:
+                        count = i
+        ind = states.index(i)
+        
+        return render_template('response.html',nval = getStateOptions(states),yval = getYearOptions(states),name = na, year = ye, 
+                              ls = str(states[i]['Metrics']['Legitimacy']['Legitimacy Score']), 
+                              pl = str(states[i]['Metrics']['Legitimacy']['Political Legitimacy']),
+                              sel = str(states[i]['Metrics']['Legitimacy']['Security Legitimacy']),
+                              el = str(states[i]['Metrics']['Legitimacy']['Economic Legitimacy']),
+                              sl = str(states[i]['Metrics']['Legitimacy']['Social Legitimacy']),
+                              es = str(states[i]['Metrics']['Effectiveness']['Effectiveness Score']),
+                              pe = str(states[i]['Metrics']['Effectiveness']['Political Effectiveness']),
+                              see = str(states[i]['Metrics']['Effectiveness']['Security Effectiveness']),
+                              ee = str(states[i]['Metrics']['Effectiveness']['Economic Effectiveness']),
+                              se = str(states[i]['Metrics']['Effectiveness']['Social Effectiveness']),
+                              sfe = str(states[i]['Metrics']['State Fragility Index"]))
 @app.route("/responsetwo")
 def render_responsetwo():
-        ins = float(request.args['inches'])
-        #The request object stores information that was sent by the client to the server.
-        #the args is a multidict
-        #the way we get info from args is that it is visible in a url. - the information in args is visible in the url for hte page being requested(ex. .../response?color=blue)
-        res = str(ins/12)
+        
         return render_template('responsetwo.html', their = request.args['inches'], response = res)
 @app.route("/responsethree")
 def render_responsethree():
